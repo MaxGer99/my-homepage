aeb548c4a217c89ae385d3fc7eeb62564c157aab
MaxGer99 <maxgerber99@gmail.com>
2025-04-16 13:10:00 -0400

    Moved and del files

---
+++ my-homepage/src/components/VideoSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const VideoSection = () => {
+	return (
+		<section>
+			<h2>Featured Videos</h2>
+			<p>Check out our latest content!</p>
+		</section>
+	);
+};
+
+export default VideoSection;
