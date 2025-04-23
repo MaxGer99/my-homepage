b5fe3ead91076978076f6f7ff56d9a8b465c43a9
MaxGer99 <maxgerber99@gmail.com>
2025-04-23 12:18:59 -0400

    Styling of footer

--- my-homepage/src/components/Footer.js
+++ my-homepage/src/components/Footer.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
 	return (
 		<footer>
-			<p>© 2025 My Homepage</p>
+			<p>© 2025 Max Gerber's Homepage</p>
+			<div className="social-icons">
+				<a
+					href="https://www.instagram.com/gerbz__/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<FaInstagram />
+				</a>
+				<a
+					href="https://www.linkedin.com/in/max-gerber-a481851b7/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<FaLinkedin />
+				</a>
+				<a
+					href="https://github.com/MaxGer99"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<FaGithub />
+				</a>
+			</div>
 		</footer>
 	);
 };
--- my-homepage/src/components/VideoSection.js
+++ my-homepage/src/components/VideoSection.js
@@ -3,7 +3,7 @@ import React from 'react';
 const VideoSection = () => {
 	return (
 		<section>
-			<h2>Featured Prjects</h2>
+			<h2>Featured Projects</h2>
 			<p>Check out my latest content!</p>
 		</section>
 	);
