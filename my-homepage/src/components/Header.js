9dce73a3106811a10f15b6b4851349863a59fa9d
MaxGer99 <maxgerber99@gmail.com>
2025-05-12 16:16:31 -0400

    Status change

--- my-homepage/src/components/Header.js
+++ my-homepage/src/components/Header.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 
-function Header({ status }) {
+function Header({ status, lastUpdated }) {
+	const [isHovered, setIsHovered] = useState(false);
+
 	return (
 		<header className="header">
-			<h1 className="header-title">{status}</h1>
+			<h1
+				className="header-title"
+				onMouseEnter={() => setIsHovered(true)}
+				onMouseLeave={() => setIsHovered(false)}
+			>
+				{status}
+				{isHovered && (
+					<span className="status-tooltip">
+						Last updated: {lastUpdated}
+					</span>
+				)}
+			</h1>
 			<a href="/resume.pdf" download className="resume-button">
 				Download Resume
 			</a>
